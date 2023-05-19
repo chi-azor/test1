a4bf1992e3a154fec67168e56bb860ee8b6e8de0
chiazor <chiazxr@gmaiil.com>
2023-05-19 09:56:28 +0100

    First commit

---
+++ site.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+	<?php
+		$phrase = "My Site";
+		$age = 30;
+		$gpa = 2.1;
+		$isMale = false;
+
+
+
+		$phrase = "Giraffe Academy";
+
+		echo strtoupper($phrase);
+	?>
+
+</body>
+</html>
